0377048909cd882907b59268d444c50500d62871
Ionela <ionela92@gmail.com>
2016-12-13 00:05:51 +0200

    arrayList

--- app/src/main/java/com/example/student/gestionarecheltuieli/MainActivity.java
+++ app/src/main/java/com/example/student/gestionarecheltuieli/MainActivity.java
@@ -2,34 +2,78 @@ package com.example.student.gestionarecheltuieli;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button scanBtn;
-    private TextView formatTxt, contentTxt;
 
+import java.util.ArrayList;
+
+
+
+
+//public class MainActivity extends AppCompactActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    private Button scanBtn;
+    private ImageButton cartButton;
+    private ArrayAdapter<String> adapter;
+    ArrayList<String> listItems=new ArrayList<String>();
+    String[] itemname = {
+            "Safari",
+            "Camera",
+            "Global",
+
+    };
+    Integer[] imgid={
+            R.drawable.ic_plus,
+            R.drawable.cart,
+            R.drawable.ic_minus,
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scanBtn = (Button)findViewById(R.id.scan_button);
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        contentTxt = (TextView)findViewById(R.id.scan_content);
-        scanBtn.setOnClickListener(this);
+        cartButton = (ImageButton)findViewById(R.id.cartButton);
 
+
+
+
+        //formatTxt = (TextView)findViewById(R.id.scan_format);
+        //contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn.setOnClickListener(this);
+        cartButton.setOnClickListener(this);
+
+        //adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, listItems);
+
+       // adapter = new ArrayAdapter<String>(this,android.R.layout.list_row, android.R.id.from_name, listItems);
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
 
 
     @Override
@@ -39,7 +83,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
+
+        if(v.getId()==R.id.cartButton){
+            System.out.println("change to cart");
+            Intent intent = new Intent(this,Cart.class);
+            startActivity(intent);
+        }
     }
+
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         //retrieve scan result
@@ -47,9 +100,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (scanningResult != null) {
             //we have a result
             String scanContent = scanningResult.getContents();
-            String scanFormat = scanningResult.getFormatName();
-            formatTxt.setText("FORMAT: " + scanFormat);
-            contentTxt.setText("CONTENT: " + scanContent);
+            //String scanFormat = scanningResult.getFormatName();
+//
+           // itemname.add(scanContent);
+//            adapter.notifyDataSetChanged();
+
 
         }else{
             Toast toast = Toast.makeText(getApplicationContext(),
@@ -57,4 +112,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             toast.show();
         }
     }
+
 }
